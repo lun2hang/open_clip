9cb2d543102b3c4dbedf69d70308bfc48f55352c
Lun <wind_lunzhang@hotmail.com>
2023-06-04 14:45:33 +0800

    ironman test

--- demo.py
+++ demo.py
@@ -6,8 +6,8 @@ import open_clip
 model, _, preprocess = open_clip.create_model_and_transforms('ViT-B-32', pretrained='laion2b_s34b_b79k')
 tokenizer = open_clip.get_tokenizer('ViT-B-32')
 
-image = preprocess(Image.open("docs/CLIP.png")).unsqueeze(0)
-text = tokenizer(["a diagram", "a dog", "a cat"])
+image = preprocess(Image.open("docs/ironman.jpeg")).unsqueeze(0)
+text = tokenizer(["a diagram", "a dog", "a cat", "a man", "a ironman"])
 
 with torch.no_grad(), torch.cuda.amp.autocast():
     image_features = model.encode_image(image)
